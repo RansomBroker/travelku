db9540599cd6761e03dfa08fdf19dd9588156c1e
RansomBroker <yadisoke@gmail.com>
2022-12-09 20:34:00 +0700

    third commit

--- resources/js/index.js
+++
@@ -1,7 +0,0 @@
-import React, {useEffect} from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
-
-if (document.getElementById('main')) {
-    ReactDOM.render(<App />, document.getElementById('main'));
-}
